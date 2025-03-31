9dc59ca309e1a9186431e149f0810f7e3b3caa2f
ilyadrugan <ilyka.dr2001@bk.ru>
2025-03-31 11:30:04 +0300

    addScoreForWatter изменено на добавление баллов только после 8 стаканов

--- store/state/watterStore/watterStore.ts
+++ store/state/watterStore/watterStore.ts
@@ -29,19 +29,19 @@ class WatterStore {
     this.addWatterToActivityData(this.watterCount);
   }
    addWatterToActivityData = async (watter: number) => {
-    if (this.watterCount === 1) {
-        await dailyActivityStore.addDailyActivity('food', dailyActivityStore.dailyTodayActivityData.food + 1);
-        await ifgScoreStore.addScore(1);
-    }
+    // if (this.watterCount === 1) {
+    //     await dailyActivityStore.addDailyActivity('food', dailyActivityStore.dailyTodayActivityData.food + 1);
+    //     await ifgScoreStore.addScore(1);
+    // }
     await dailyActivityStore.addDailyActivity('watter', watter);
   };
     addScoreForWatter = async () => {
       // setIsLoadingButton(true)
       this.isLoading = true;
       dailyActivityStore.addDailyActivity('watter', 8);
-      dailyActivityStore.addDailyActivity('food', dailyActivityStore.dailyTodayActivityData.food + 2);
+      dailyActivityStore.addDailyActivity('food', dailyActivityStore.dailyTodayActivityData.food + 3);
       await dailyActivityStore.addDailyActivity('isDrinkEight', true);
-      await ifgScoreStore.addScore(2);
+      await ifgScoreStore.addScore(3);
       this.isLoading = false;
       this.isDrinkEight = true;
       // setIsLoadingButton(false)
